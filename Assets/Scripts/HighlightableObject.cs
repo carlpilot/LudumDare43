9b58a7ceacac8d7206eea9d74d83b530547dc288
carlpilot <carlbert101@gmail.com>
2018-12-01 11:25:15 -0500

    initial commit

---
+++ Assets/Scripts/HighlightableObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightableObject : MonoBehaviour {
+
+    public Material highlightedMaterial;
+    private Material[] materials;
+
+    public bool isHighlighted;
+
+	void Awake () {
+        materials = GetComponent<MeshRenderer> ().sharedMaterials;
+	}
+	
+	void Update () {
+		if(isHighlighted) {
+            Material[] m2 = new Material[GetComponent<MeshRenderer> ().sharedMaterials.Length];
+            for(int i = 0; i < GetComponent<MeshRenderer> ().sharedMaterials.Length; i++) {
+                m2[i] = highlightedMaterial;
+            }
+            GetComponent<MeshRenderer> ().sharedMaterials = m2;
+        } else {
+            GetComponent<MeshRenderer> ().sharedMaterials = materials;
+        }
+	}
+}
---
+++ Assets/Scripts/Highlighter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Highlighter : MonoBehaviour {
+
+	void Update () {
+        RaycastHit hit;
+	    if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 10f)) {
+            Debug.Log (hit.transform.name);
+            if(hit.transform.tag.Equals("Highlightable")) {
+                hit.transform.GetComponent<HighlightableObject> ().isHighlighted = true;
+            }
+        }
+        foreach (GameObject g in GameObject.FindGameObjectsWithTag ("Highlightable")) {
+            if (hit.transform != null && g != hit.transform.gameObject) {
+                g.GetComponent<HighlightableObject> ().isHighlighted = false;
+            }
+        }
+    }
+}
